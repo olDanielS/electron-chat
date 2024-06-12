92d20bb6a642e6ef5149529275627fe91df570d6
olDaniels <danielnrt80@gmail.com>
2024-06-11 23:13:03 -0300

    initial config

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="bg-gray-800">
+        <h1 className="text-red-800">Hello world</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App;
